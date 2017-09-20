e70b3c405d8c631477aadfd159237f5668d13706
dadstache <darrickly@gmail.com>
2017-09-20 16:41:54 -0700

    adding new lines

--- parser101.py
+++ parser101.py
@@ -5,15 +5,19 @@ from bs4 import BeautifulSoup
 soup = BeautifulSoup("<html><p>This is <b>invalid HTML</p></html>", "html.parser")
 print(soup)
 # <html><p>This is <b>invalid HTML</b></p></html>
+\n
 
 soup = BeautifulSoup("<html><p>This is <b>invalid HTML</p></html>", "lxml")
 print(soup)
 # <html><body><p>This is <b>invalid HTML</b></p></body></html>
 
+\n
+
 soup = BeautifulSoup("<html><p>This is <b>invalid HTML</p></html>", "xml")
 print(soup)
 # <?xml version="1.0" encoding="utf-8"?>
 # <html><p>This is <b>invalid HTML</b></p></html>
+\n
 
 soup = BeautifulSoup("<html><p>This is <b>invalid HTML</p></html>", "html5lib")
 print(soup)
